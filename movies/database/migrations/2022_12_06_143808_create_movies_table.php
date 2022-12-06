dcf08205c4e9078cbe7fd6a6273935c33c6c9cf8
milanche97 <milanrbajic@gmail.com>
2022-12-06 15:49:54 +0100

    drugi komit

---
+++ movies/database/migrations/2022_12_06_143808_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('director');
+            $table->string('year');
+            $table->string('storyline');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
