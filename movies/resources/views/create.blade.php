d318142030f96c5fae46d537397b2cfebd9b581e
milanche97 <milanrbajic@gmail.com>
2022-12-07 16:34:10 +0100

    komit sa komentarima

--- movies/resources/views/create.blade.php
+++ movies/resources/views/create.blade.php
@@ -8,24 +8,24 @@
 
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Title</label>
-    <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Avatar">
+    <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Avatar"><br>
 </div>
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Genre</label>
-    <input type="text" name="genre" class="form-control" id="exampleFormControlInput1" placeholder="Sci-Fi">
+    <input type="text" name="genre" class="form-control" id="exampleFormControlInput1" placeholder="Sci-Fi"><br>
 </div>
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Director</label>
-    <input type="text" name="director" class="form-control" id="exampleFormControlInput1" placeholder="Christoper Nolan">
+    <input type="text" name="director" class="form-control" id="exampleFormControlInput1" placeholder="Christoper Nolan"><br>
 </div>
 
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Year</label>
-    <input type="int" name="year" class="form-control" id="exampleFormControlInput1" placeholder="2012">
+    <input type="int" name="year" class="form-control" id="exampleFormControlInput1" placeholder="2012"><br>
 </div>
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Storyline</label>
-    <input type="text" name="storyline" class="form-control" id="exampleFormControlInput1" placeholder="/">
+    <input type="text" name="storyline" class="form-control" id="exampleFormControlInput1" placeholder=""><br>
 </div>
 
     <button type="submit">Add movie</button>
